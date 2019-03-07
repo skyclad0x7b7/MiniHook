8647bef9342be64a455b0e610b0c1dccee45f0c2
전상현 <skyclad0x7b7@gmail.com>
2019-03-08 08:37:21 +0900

    fread 함수에 restrict 키워드 추가, uname 인자 버그 수정

--- hook.c
+++ hook.c
@@ -7,7 +7,7 @@ lib_fopen_type lib_fopen = NULL;
 typedef size_t(*lib_fwrite_type)(const void *, size_t, size_t, FILE *);
 lib_fwrite_type lib_fwrite = NULL;
 
-typedef size_t(*lib_fread_type)(const void *, size_t, size_t, FILE *);
+typedef size_t(*lib_fread_type)(const void *restrict, size_t, size_t, FILE *restrict);
 lib_fread_type lib_fread = NULL;
 
 typedef int(*lib_fclose_type)(FILE *);
@@ -126,7 +126,7 @@ size_t fwrite(const void *buffer, size_t size, size_t count, FILE *stream)
     return ret;
 }
 
-size_t fread(const void *buffer, size_t size, size_t count, FILE *stream)
+size_t fread(const void *restrict buffer, size_t size, size_t count, FILE *restrict stream)
 {
     Variable args[4] = { 
         { VT_string, (int)buffer },
@@ -186,7 +186,7 @@ mode_t uname(mode_t mode)
         { VT_unsigned_int, (int)mode }
     };
     HOOK_LOG(LT_FILE, "uname", 1, args);
-    mode_t ret = lib_uname(oldname, newname);
+    mode_t ret = lib_uname(mode);
     return ret;
 }
 
