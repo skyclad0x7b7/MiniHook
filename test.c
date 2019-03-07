1055bcdfe7ce0408d8589cfa3cf742f112d55acf
전상현 <skyclad0x7b7@gmail.com>
2019-03-07 15:19:43 +0900

    fclose에 문자열 넘기던 버그 수정;;

--- test.c
+++ test.c
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
     const char *testString = "Hello World!";
     FILE *hFile = fopen("hiddenFile.txt", "wb");
     fwrite(testString, sizeof(char), strlen(testString), hFile);
-    fclose(testString);
+    fclose(hFile);
     return 0;
 }
