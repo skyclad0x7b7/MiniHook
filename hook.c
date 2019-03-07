9c3bacabe8e05292976d8c1020c7d580c0feedde
전상현 <skyclad0x7b7@gmail.com>
2019-03-07 15:26:31 +0900

    VT_unsigned_int 추가, fwrite 후킹 추가

--- hook.c
+++ hook.c
@@ -4,10 +4,14 @@
 typedef FILE *(*lib_fopen_type)(const char *, const char *);
 lib_fopen_type lib_fopen = NULL;
 
+typedef size_t(*lib_fwrite_type)(const void *base, size_t size, size_t count, FILE *fp);
+lib_fwrite_type lib_fwrite = NULL;
+
 static void con() __attribute__((constructor));
 void con()
 {
     lib_fopen = (lib_fopen_type)dlsym(RTLD_NEXT, "fopen");
+    lib_fwrite = (lib_fwrite_type)dlsym(RTLD_NEXT, "fwrite");
 }
 /************************************/
 
@@ -20,6 +24,10 @@ void HOOK_MAKE_LOG_STRING(char *buffer, const char *func_name, const unsigned in
     {
         switch(args[i]._type)
         {
+        case VT_unsigned_int:
+        {
+            snprintf(temp, 1024, "%u", (unsigned)(args[i]._var));
+        }
         case VT_string:
         {
             snprintf(temp, 1024, "\"%s\"", (char *)(args[i]._var));
@@ -59,7 +67,7 @@ void HOOK_LOG(const LoggingType loggingType, const char *func_name, const unsign
             FILE *hFile = lib_fopen("log.txt", "a");
             if(hFile != NULL)
             {
-                fwrite(buffer, sizeof(char), strlen(buffer), hFile);
+                lib_fwrite(buffer, sizeof(char), strlen(buffer), hFile);
                 fclose(hFile);    
             }
             break;
@@ -77,5 +85,18 @@ FILE *fopen(const char *filename, const char *mode)
     };
     HOOK_LOG(LT_FILE, "fopen", 2, args);
     FILE *ret = lib_fopen(filename, mode);
+    return ret;
+}
+
+size_t fwrite(const char *buffer, size_t size, size_t count, FILE *stream)
+{
+     Variable args[4] = { 
+        { VT_string, (int)buffer },
+        { VT_unsigned_int, (int)size },
+        { VT_unsigned_int, (int)count },
+        { VT_buffer, (int)stream }
+    };
+    HOOK_LOG(LT_FILE, "fwrite", 4, args);
+    size_t ret = lib_fwrite(buffer, size, count, stream);
     return ret;
 }
